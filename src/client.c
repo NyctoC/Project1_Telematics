e7fe90a1988e674d86818262d921478dc5e1d38e
NyctoC <camilocardenas2705@gmail.com>
2024-10-17 22:15:10 -0500

    New Working Base DHCP server

--- src/client.c
+++ src/client.c
@@ -1,162 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
-#include <pthread.h>
 
-#define DHCP_SERVER_PORT 67 // DHCP server port
-#define DHCP_CLIENT_PORT 68 // DHCP client port
-#define MAXBUFLEN 512  // Maximum buffer size for receiving data
+#define DHCP_SERVER_IP "10.0.2.5"
+#define DHCP_SERVER_PORT 67
+#define DHCP_CLIENT_PORT 68
+#define BUFFER_SIZE 1024
+#define IP_POOL_SIZE 10
+#define LEASE_TIME 3600 // 1 hour
 
-// DHCP packet structure (simplified)
-struct dhcp_packet {
-    uint8_t op;         // Message type
-    uint8_t htype;      // Hardware address type
-    uint8_t hlen;       // Hardware address length
-    uint8_t hops;       // Hops
-    uint32_t xid;       // Transaction ID
-    uint16_t secs;      // Seconds elapsed
-    uint16_t flags;     // Flags
-    uint32_t ciaddr;    // Client IP address
-    uint32_t yiaddr;    // Your IP address (server fills this)
-    uint32_t siaddr;    // Next server IP address
-    uint32_t giaddr;    // Relay agent IP address
-    unsigned char chaddr[16]; // Client hardware address
-    char sname[64];     // Server name
-    char file[128];     // Boot filename
-    unsigned char options[312]; // Optional parameters (DHCP options)
-};
-
-// Function to create and send DHCPDISCOVER
-void send_dhcpdiscover(int sockfd, struct sockaddr_in *server_addr, uint32_t xid) {
-    struct dhcp_packet discover_packet;
-    memset(&discover_packet, 0, sizeof(discover_packet));
-
-    discover_packet.op = 1;  // DHCPREQUEST
-    discover_packet.htype = 1;  // Ethernet
-    discover_packet.hlen = 6;   // MAC length
-    discover_packet.xid = xid;  // Unique transaction ID
-    discover_packet.flags = htons(0x8000); // Broadcast flag
-
-    // DHCP options: magic cookie + message type (DHCPDISCOVER)
-    discover_packet.options[0] = 0x63;
-    discover_packet.options[1] = 0x82;
-    discover_packet.options[2] = 0x53;
-    discover_packet.options[3] = 0x63;
-    discover_packet.options[4] = 53;  // DHCP Message Type option
-    discover_packet.options[5] = 1;   // Length
-    discover_packet.options[6] = 1;   // DHCPDISCOVER
-
-    if (sendto(sockfd, &discover_packet, sizeof(discover_packet), 0,
-               (struct sockaddr *)server_addr, sizeof(*server_addr)) == -1) {
-        perror("sendto");
-        exit(1);
-    }
-
-    printf("DHCP client: Sent DHCPDISCOVER\n");
+void request_renewal(int sock, struct sockaddr_in *server_addr) {
+    // Prepare DHCPREQUEST message (simplified)
+    char dhcp_request[BUFFER_SIZE];
+    strcpy(dhcp_request, "DHCPREQUEST");
+    
+    // Send DHCPREQUEST message
+    sendto(sock, dhcp_request, strlen(dhcp_request), 0, (struct sockaddr *)server_addr, sizeof(*server_addr));
+    
+    // Receive DHCPACK message
+    char buffer[BUFFER_SIZE];
+    recvfrom(sock, buffer, BUFFER_SIZE, 0, NULL, NULL);
+    printf("Received: %s\n", buffer);
 }
 
-// Function to receive and process DHCPOFFER
-void receive_dhcpoffer(int sockfd) {
+void release_ip(int sock, struct sockaddr_in *server_addr) {
+    // Prepare DHCPRELEASE message (simplified)
+    char dhcp_release[BUFFER_SIZE];
+    strcpy(dhcp_release, "DHCPRELEASE");
+    
+    // Send DHCPRELEASE message
+    sendto(sock, dhcp_release, strlen(dhcp_release), 0, (struct sockaddr *)server_addr, sizeof(*server_addr));
+}
+
+int main() {
+    int sock;
     struct sockaddr_in server_addr;
-    socklen_t addr_len = sizeof(server_addr);
-    struct dhcp_packet offer_packet;
-    int numbytes;
+    char dhcp_discover[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
 
-    if ((numbytes = recvfrom(sockfd, &offer_packet, sizeof(offer_packet), 0,
-                             (struct sockaddr *)&server_addr, &addr_len)) == -1) {
-        perror("recvfrom");
-        exit(1);
+    // Create UDP socket
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    printf("DHCP client: Received DHCPOFFER\n");
-
-    // Print offered IP address and other relevant information
-    struct in_addr yiaddr;
-    yiaddr.s_addr = offer_packet.yiaddr;
-    printf("Offered IP Address: %s\n", inet_ntoa(yiaddr));
-
-    // Additional information like subnet mask, gateway, etc., can be extracted from options
-}
-
-// Thread function for handling the renewal of the IP address
-void *renew_ip(void *arg) {
-    int sockfd = *(int *)arg;
-    struct sockaddr_in server_addr;
-    uint32_t xid = rand(); // Generate a new transaction ID
-
-    while (1) {
-        sleep(60);  // Wait for a certain time (e.g., 60 seconds) before renewing IP
-
-        // Send a DHCPREQUEST to renew the IP
-        send_dhcpdiscover(sockfd, &server_addr, xid);
-        receive_dhcpoffer(sockfd);
-    }
-
-    return NULL;
-}
-
-// Main function
-int main(void) {
-    int sockfd;
-    struct sockaddr_in client_addr, server_addr;
-    uint32_t xid = rand();  // Generate a random transaction ID
-
-    // Create a UDP socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
-    }
-
-    // Setup the client address structure
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(DHCP_CLIENT_PORT);
-    client_addr.sin_addr.s_addr = INADDR_ANY;
-    memset(client_addr.sin_zero, '\0', sizeof client_addr.sin_zero);
-
-    // Bind the socket to the client port
-    if (bind(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)) == -1) {
-        perror("bind");
-        close(sockfd);
-        exit(1);
-    }
-
-    // Setup the server address structure
+    // Set up the server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(DHCP_SERVER_PORT);
-    server_addr.sin_addr.s_addr = INADDR_BROADCAST;  // Broadcast address
+    inet_pton(AF_INET, DHCP_SERVER_IP, &server_addr.sin_addr);
 
-    // Allow broadcasting
-    int broadcast = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) == -1) {
-        perror("setsockopt");
-        close(sockfd);
-        exit(1);
+    // Prepare DHCPDISCOVER message (simplified)
+    strcpy(dhcp_discover, "DHCPDISCOVER");
+
+    // Send DHCPDISCOVER message
+    ssize_t bytes_sent = sendto(sock, dhcp_discover, strlen(dhcp_discover), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (bytes_sent < 0) {
+        perror("sendto failed");
+        close(sock);
+        exit(EXIT_FAILURE);
     }
 
-    // Send DHCPDISCOVER
-    send_dhcpdiscover(sockfd, &server_addr, xid);
-
-    // Receive DHCPOFFER
-    receive_dhcpoffer(sockfd);
-
-    // Create a thread to handle the IP renewal process
-    pthread_t renew_thread;
-    if (pthread_create(&renew_thread, NULL, renew_ip, &sockfd) != 0) {
-        perror("pthread_create");
-        close(sockfd);
-        exit(1);
+    // Receive DHCPOFFER message
+    ssize_t bytes_received = recvfrom(sock, buffer, BUFFER_SIZE, 0, NULL, NULL);
+    if (bytes_received < 0) {
+        perror("recvfrom failed");
+        close(sock);
+        exit(EXIT_FAILURE);
     }
 
-    // Wait for the thread to finish (in this simple implementation, it will run indefinitely)
-    pthread_join(renew_thread, NULL);
+    // Print received DHCPOFFER
+    printf("Received: %s\n", buffer);
 
-    // Close the socket and exit
-    close(sockfd);
+    // Check for renewal condition
+    while (1) {
+        sleep(LEASE_TIME / 2); // Example: request renewal halfway through the lease time
+        request_renewal(sock, &server_addr);
+    }
 
+    // Release IP before exit
+    release_ip(sock, &server_addr);
+    close(sock);
+    return 0;
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,196 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <pthread.h>
 
-#define PORT 67
-#define MAXBUFLEN 512
-#define MAX_CLIENTS 100
-#define LEASE_TIME 3600  // Lease time in seconds
+#define DHCP_SERVER_PORT 67
+#define DHCP_CLIENT_PORT 68
+#define BUFFER_SIZE 1024
 
-struct dhcp_packet {
-    uint8_t op;
-    uint8_t htype;
-    uint8_t hlen;
-    uint8_t hops;
-    uint32_t xid;
-    uint16_t secs;
-    uint16_t flags;
-    uint32_t ciaddr;
-    uint32_t yiaddr;
-    uint32_t siaddr;
-    uint32_t giaddr;
-    unsigned char chaddr[16];
-    char sname[64];
-    char file[128];
-    unsigned char options[312];
-};
+#define IP_POOL_SIZE 10
+#define LEASE_TIME 3600 // 1 hour
 
-// Structure to keep track of IP lease information
-struct ip_lease {
-    uint32_t ip;
-    unsigned char mac[16];
-    time_t lease_start;
-};
+typedef struct {
+    char ip[16]; // IP address in string format
+    time_t lease_time; // Timestamp of when the lease was granted
+    int is_allocated; // 0 if free, 1 if allocated
+} IpLease;
 
-// Array to store IP leases
-struct ip_lease leases[MAX_CLIENTS];
-int lease_count = 0;
-uint32_t available_ips[MAX_CLIENTS];  // Store available IPs
-int available_ip_count = 0;
-pthread_mutex_t lease_mutex = PTHREAD_MUTEX_INITIALIZER;
+IpLease ip_pool[IP_POOL_SIZE]; // Array to store IP leases
 
-// Initialize available IP pool (hardcoded for simplicity)
 void init_ip_pool() {
-    available_ip_count = 5;
-    available_ips[0] = inet_addr("192.168.1.100");
-    available_ips[1] = inet_addr("192.168.1.101");
-    available_ips[2] = inet_addr("192.168.1.102");
-    available_ips[3] = inet_addr("192.168.1.103");
-    available_ips[4] = inet_addr("192.168.1.104");
+    for (int i = 0; i < IP_POOL_SIZE; i++) {
+        sprintf(ip_pool[i].ip, "192.168.1.%d", i + 1); // Example subnet
+        ip_pool[i].is_allocated = 0;
+    }
 }
 
-void print_mac_address(unsigned char *mac, size_t len) {
-    printf("Client MAC Address: ");
-    for (size_t i = 0; i < len; i++) {
-        printf("%02x", mac[i]);
-        if (i < len - 1) printf(":");
-    }
-    printf("\n");
-}
-
-// Check if a MAC address already has a lease
-int find_lease_by_mac(unsigned char *mac) {
-    for (int i = 0; i < lease_count; i++) {
-        if (memcmp(mac, leases[i].mac, 16) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-// Allocate an IP address from the available pool
-uint32_t allocate_ip(unsigned char *mac) {
-    pthread_mutex_lock(&lease_mutex);
-    if (available_ip_count > 0) {
-        int index = find_lease_by_mac(mac);
-        if (index != -1) {
-            // Client already has a lease
-            pthread_mutex_unlock(&lease_mutex);
-            return leases[index].ip;
-        }
-
-        uint32_t ip = available_ips[--available_ip_count];  // Allocate last available IP
-        struct ip_lease lease;
-        lease.ip = ip;
-        memcpy(lease.mac, mac, 16);
-        lease.lease_start = time(NULL);
-        leases[lease_count++] = lease;
-
-        pthread_mutex_unlock(&lease_mutex);
-        return ip;
-    }
-    pthread_mutex_unlock(&lease_mutex);
-    return 0;  // No available IPs
-}
-
-void send_dhcpoffer(int sockfd, struct dhcp_packet *discover_packet, struct sockaddr_in *client_addr) {
-    struct dhcp_packet offer_packet;
-    memset(&offer_packet, 0, sizeof(offer_packet));
-
-    offer_packet.op = 2;
-    offer_packet.htype = discover_packet->htype;
-    offer_packet.hlen = discover_packet->hlen;
-    offer_packet.xid = discover_packet->xid;
-    offer_packet.flags = discover_packet->flags;
-    offer_packet.yiaddr = allocate_ip(discover_packet->chaddr);
-    if (offer_packet.yiaddr == 0) {
-        printf("No available IP addresses to offer\n");
-        return;
-    }
-    offer_packet.siaddr = inet_addr("192.168.1.1");
-    memcpy(offer_packet.chaddr, discover_packet->chaddr, 16);
-
-    // DHCP options (simplified)
-    offer_packet.options[0] = 0x63;
-    offer_packet.options[1] = 0x82;
-    offer_packet.options[2] = 0x53;
-    offer_packet.options[3] = 0x63;
-
-    offer_packet.options[4] = 53;
-    offer_packet.options[5] = 1;
-    offer_packet.options[6] = 2;
-
-    if (sendto(sockfd, &offer_packet, sizeof(offer_packet), 0, (struct sockaddr *)client_addr, sizeof(*client_addr)) == -1) {
-        perror("sendto");
-    }
-
-    printf("Sent DHCPOFFER to client\n");
-}
-
-void *handle_dhcp_request(void *arg) {
-    int sockfd = *(int *)arg;
+void *handle_client(void *arg) {
+    int sock = *(int *)arg;
     struct sockaddr_in client_addr;
-    socklen_t addr_len;
-    char buf[MAXBUFLEN];
-    int numbytes;
-    struct dhcp_packet *dhcp;
-
-    addr_len = sizeof client_addr;
-    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1, 0, (struct sockaddr *)&client_addr, &addr_len)) == -1) {
-        perror("recvfrom");
-        pthread_exit(NULL);
-    }
-
-    buf[numbytes] = '\0';
-    dhcp = (struct dhcp_packet *)buf;
-
-    if (dhcp->op == 1) {
-        printf("Received DHCPDISCOVER\n");
-        print_mac_address(dhcp->chaddr, dhcp->hlen);
-        send_dhcpoffer(sockfd, dhcp, &client_addr);
-    } else {
-        printf("Received non-DHCPDISCOVER message\n");
-    }
-
-    pthread_exit(NULL);
-}
-
-int main(void) {
-    int sockfd;
-    struct sockaddr_in server_addr;
-    pthread_t threads[MAX_CLIENTS];
-    int thread_count = 0;
-
-    init_ip_pool();
-
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    memset(server_addr.sin_zero, '\0', sizeof server_addr.sin_zero);
-
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("bind");
-        close(sockfd);
-        exit(1);
-    }
-
-    printf("DHCP server: waiting for requests...\n");
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
 
     while (1) {
-        pthread_create(&threads[thread_count++], NULL, handle_dhcp_request, (void *)&sockfd);
-        if (thread_count >= MAX_CLIENTS) thread_count = 0;
+        // Receive DHCPDISCOVER message
+        ssize_t bytes_received = recvfrom(sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
+        if (bytes_received < 0) {
+            perror("recvfrom failed");
+            continue;
+        }
+
+        // Process DHCPDISCOVER (simplified)
+        printf("Received DHCPDISCOVER from %s\n", inet_ntoa(client_addr.sin_addr));
+
+        // Prepare DHCPOFFER message (simplified)
+        char dhcp_offer[BUFFER_SIZE];
+        memset(dhcp_offer, 0, sizeof(dhcp_offer));
+        strcpy(dhcp_offer, "DHCPOFFER");
+
+        // Ensure the message is null-terminated
+        dhcp_offer[strlen(dhcp_offer)] = '\0';
+
+        // Send DHCPOFFER message
+        ssize_t bytes_sent = sendto(sock, dhcp_offer, strlen(dhcp_offer), 0, (struct sockaddr *)&client_addr, client_len);
+        if (bytes_sent < 0) {
+            perror("sendto failed");
+        } else {
+            printf("Sent DHCPOFFER to %s\n", inet_ntoa(client_addr.sin_addr));
+        }
     }
 
-    close(sockfd);
+    // Add code to handle DHCPREQUEST and other messages
+
+    close(sock);
+    return NULL;
+}
+
+int main() {
+    init_ip_pool();
+
+    int sock;
+    struct sockaddr_in server_addr;
+
+    // Create UDP socket
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the socket to the DHCP server port
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(DHCP_SERVER_PORT);
+
+    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("DHCP server listening on port %d...\n", DHCP_SERVER_PORT);
+
+    // Create a thread to handle clients
+    pthread_t tid;
+    pthread_create(&tid, NULL, handle_client, &sock);
+    pthread_detach(tid);
+
+    // Keep the main thread running
+    while (1) {
+        sleep(1);
+    }
+
     return 0;
 }
